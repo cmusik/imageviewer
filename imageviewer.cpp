2c02010ed6c8cc50b4dd83115fe30df163813800
christof@ilja.athome.local <>
2007-07-01 01:12:40 +0200

    * added mouse and keyboard support * added displaying all images from current dir

--- imageviewer.cpp
+++ imageviewer.cpp
@@ -1,30 +1,103 @@
 #include <QDebug>
+#include <QPixmap>
 
 #include "imageviewer.h"
+enum {
+	NEXT = 1,
+	PREV = -1
+};
 
 ImageViewer::ImageViewer(QString file, QWidget *parent) : QWidget(parent) {
 	setupUi(this);
+
+	QFileInfo info = QFileInfo(file);
+	m_current_image = info.fileName();
+
+	m_dir = new QDir(info.absolutePath());
+	m_list = m_dir->entryList(QStringList() << "*.jpg" << "*.png");
 
 	setWindowState(Qt::WindowFullScreen);
 	setWindowFlags(Qt::Popup);
 	setAttribute(Qt::WA_QuitOnClose);
 
-	QImage img = QImage(file);
+	show();
+	openImage();
+}
 
-	//img = img.scaledToWidth(width(), Qt::SmoothTransformation);
+void ImageViewer::openImage() {
+	lbl_image->setCursor(Qt::WaitCursor);
+	QImage img = QImage(m_dir->path()+"/"+m_current_image);
 
-
-	if (img.width()/width() < 0) {
-		img = img.scaledToWidth(width(), Qt::SmoothTransformation);
+	if (img.width()/img.height() > 0) {
+		if (img.width()/width() > 0) {
+			img = img.scaledToWidth(width(), Qt::SmoothTransformation);
+		}
+		else /*if (img.height() > height())*/ {
+			img = img.scaledToHeight(height(), Qt::SmoothTransformation);
+		}
 	}
-	else /*if (img.height() > height())*/ {
-		img = img.scaledToHeight(height(), Qt::SmoothTransformation);
+	else {
+		if (img.width()/width() < 0) {
+			img = img.scaledToWidth(width(), Qt::SmoothTransformation);
+		}
+		else /*if (img.height() > height())*/ {
+			img = img.scaledToHeight(height(), Qt::SmoothTransformation);
+		}
 	}
-
-
-
 
 	lbl_image->setPixmap(QPixmap::fromImage(img));
+	lbl_image->setCursor(Qt::ArrowCursor);
+}
 
-	show();
+void ImageViewer::find(int delta) {
+	int next = m_list.indexOf(QRegExp("^"+m_current_image+"$")) + delta;
+	if (next > m_list.count()-1)
+		next = 0;
+
+	if (next < 0)
+		next = m_list.count()-1;
+
+	m_current_image = m_list.at(next);
+	openImage();
+}
+
+void ImageViewer::wheelEvent(QWheelEvent *e) {
+	if (e->delta() > 0) {
+		find(PREV);
+	}
+	else {
+		find(NEXT);
+	}
+}
+
+void ImageViewer::keyPressEvent(QKeyEvent *e) {
+	switch(e->key()) {
+		case Qt::Key_Space:
+			find(NEXT);
+			break;
+
+		case Qt::Key_Backspace:
+			find(PREV);
+			break;
+
+		case Qt::Key_Escape:
+			close();
+			break;
+
+		default:
+			e->ignore();
+	}
+}
+
+void ImageViewer::mouseReleaseEvent(QMouseEvent *e) {
+	switch (e->button()) {
+		case Qt::LeftButton:
+			find(NEXT);
+			break;
+		case Qt::RightButton:
+			close();
+			break;
+		default:
+			e->ignore();
+	}
 }
--- imageviewer.h
+++ imageviewer.h
@@ -5,8 +5,8 @@
 #include <QLabel>
 #include <QImage>
 #include <QString>
-#include <QPixmap>
-#include <QPalette>
+#include <QDir>
+#include <QWheelEvent>
 #include "ui_imageviewer.h"
 
 class ImageViewer : public QWidget, private Ui::ImageViewer {
@@ -14,6 +14,17 @@ class ImageViewer : public QWidget, private Ui::ImageViewer {
 
 	public:
 		ImageViewer(QString, QWidget* = 0);
+
+	private:
+		void find(int);
+		void openImage();
+		void wheelEvent(QWheelEvent*);
+		void keyPressEvent(QKeyEvent*);
+		void mouseReleaseEvent(QMouseEvent*);
+
+		QDir *m_dir;
+		QStringList m_list;
+		QString m_current_image;
 };
 
 #endif
